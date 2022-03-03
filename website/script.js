fdbe656d16b1722db481db4f6d46ae358a46ffc1
BillSkills <billskills01@gmail.com>
2022-03-03 17:47:30 -0500

    modified:   website/index.html 	deleted:    website/references.txt 	deleted:    website/script.js 	modified:   website/subpages/references.html 	deleted:    website/subpages/template.html

--- website/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  if (localStorage.getItem("index_scroll") != null) {
-    $(window).scrollTop(localStorage.getItem("index_scroll"));
-  }
-
-  $(window).on("scroll", function () {
-    localStorage.setItem("index_scroll", $(window).scrollTop());
-  });
-});
